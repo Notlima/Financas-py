203561d4fd7c60662ccd090d959f81f4f028c7e7
Amilton Castro <amiltonscjunior@gmail.com>
2025-04-11 22:27:54 +0100

    Create date change, working balance

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template, send_from_directory
 from database import db, init_app
 from models import Transaction
 from datetime import datetime
+from sqlalchemy import func
 
 app = Flask(__name__)
 
@@ -23,16 +24,17 @@ def add_transaction():
   data = request.json
   valor = float(data["valor"])
   categoria = data["categoria"]
-  print(f"Recebido - Valor: {valor}, Categoria: {categoria}")
-
+  
+  data_transacao = datetime.fromisoformat(data.get("data", datetime.now().isoformat()))
   #Saláro é possitivo, despesas negativas
   valor_final = valor if categoria == "salario" else -valor
 
-  nova_transacao = Transaction(valor=valor_final, categoria=categoria)
+  nova_transacao = Transaction(
+    valor=valor_final, 
+    categoria=categoria,
+    data=data_transacao)
   db.session.add(nova_transacao)
   db.session.commit()
-
-  print(f"Salvo no BD - Valor: {nova_transacao.valor}")
 
   return jsonify({
     "status": "success", 
@@ -43,6 +45,27 @@ def add_transaction():
 def get_transaction():
   transacoes = Transaction.query.all()
   return jsonify([t.to_dict() for t in transacoes])
+
+@app.route("/transactions/summary", methods=["GET"])
+def get_summary():
+    today = datetime.now()
+    current_mont = today.month
+    current_year = today.year
+    
+    transactions = Transaction.query.filter(
+        func.extract('month', Transaction.data) == current_mont,
+        func.extract('year', Transaction.data) == current_year
+    ).all()
+    
+    income = sum(t.valor for t in transactions if t.valor > 0)
+    outcome = abs(sum(t.valor for t in transactions if t.valor < 0))
+    balance = income - outcome
+    
+    return jsonify({
+        "entradas": income,
+        "saidas": outcome,
+        "saldo": balance
+    })
 
 if __name__ == "__main__":
   app.run(debug=True)
